9b5862e2fe461d4072585f73cac7573c87004d0d
codefrontdev <cd62970@gmail.com>
2024-10-16 15:36:19 +0200

    Update all

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,23 +1,23 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import User from "../models/UserModel";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import Wallet from "../models/WalletModel";
 import { sendEmail } from "./../../utils/sendEmail";
+import { ApiError } from "../../utils/APIError";
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
 // @access  Public
-const register = asyncHandler(async (req: Request, res: Response) => {
+const register = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   console.log(req.body);
   try {
     // TODO: Implement authentication logic here
     const { fullName, email, password } = req.body;
     const user = await User.findOne({ email });
     if (user) {
-      res.status(400).json({ message: "User already exists" });
-      return;
+      return next(new ApiError("User already exists", 400));
     }
 
     const token = Math.floor(100000 + Math.random() * 900000);
@@ -78,60 +78,55 @@ const register = asyncHandler(async (req: Request, res: Response) => {
 
     res.status(201).json({ message: "User signed in successfully" });
   } catch (error) {
-    res.status(500).json({ error: error });
+    return next(new ApiError(`Error registering user: ${error}`, 500));
   }
 });
 
 // @desc    Login user
 // @route   POST /api/v1/auth/login
 // @access  Public
-const login = asyncHandler(async (req: Request, res: Response) => {
+const login = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
 
     if (!user) {
-      res.status(400).json({ message: "User not found" });
-      return;
+      return next(new ApiError("Invalid credentials", 400));
     }
 
     if (!user.verified) {
-      res.status(400).json({ message: "User not verified" });
-      return;
+      return next(new ApiError("User not verified", 400));
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      res.status(400).json({ message: "Invalid credentials" });
-      return;
+      return next(new ApiError("Invalid credentials", 400));
     }
 
     res.status(200).json({ message: "User logged in successfully", user });
   } catch (error) {
-    res.status(500).json({ error: error });
+    return next(new ApiError(`Error logging in user: ${error}`, 500));
   }
 });
 
 // @desc    Verify user
 // @route   POST /api/v1/auth/verify
 // @access  Public
-const verifyUser = asyncHandler(async (req: Request, res: Response) => {
+const verifyUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { token } = req.body;
     
     const user = await User.findById(req.params.id);
 
     if (!user) {
-      res.status(400).json({ message: "User not found" });
-      return;
+      return next(new ApiError("User not found", 400));
     }
 
     const corecteToken = token === user.token;
 
     if (user.verified) {
-      res.status(400).json({ message: "User already verified" });
-      return;
+      return next(new ApiError("User already verified", 400));
     }
 
     if (
@@ -190,8 +185,7 @@ const verifyUser = asyncHandler(async (req: Request, res: Response) => {
 
       sendEmail(verifyEmailHtml, "verify your email", user.email);
       await user.save();
-      res.status(400).json({ message: "Token expired", link });
-      return;
+      return next(new ApiError("Token expired", 400));
     }
     user.verified = true;
     user.token = null;
@@ -200,20 +194,19 @@ const verifyUser = asyncHandler(async (req: Request, res: Response) => {
 
     res.status(200).json({ message: "User verified successfully" });
   } catch (error) {
-    res.status(500).json({ error: error });
+    return next(new ApiError(`Error verifying user: ${error}`, 500));
   }
 });
 
 // @desc    Forgot password
 // @route   POST /api/v1/auth/forgot-password
 // @access  Public
-const forgotPassword = asyncHandler(async (req: Request, res: Response) => {
+const forgotPassword = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      res.status(400).json({ message: "User not found" });
-      return;
+      return next(new ApiError("User not found", 400));
     }
 
     const token = crypto.randomBytes(32).toString("hex");
@@ -268,26 +261,24 @@ const forgotPassword = asyncHandler(async (req: Request, res: Response) => {
 
     res.status(200).json({ message: "Password reset link sent successfully" });
   } catch (error) {
-    res.status(500).json({ error: error });
+    return next(new ApiError(`Error resetting password: ${error}`, 500));
   }
 });
 
 // @desc    Reset password
 // @route   POST /api/v1/auth/reset-password/:id/:token
 // @access  Public
-const resetPassword = asyncHandler(async (req: Request, res: Response) => {
+const resetPassword = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const { id, token } = req.params;
   const { password } = req.body;
   try {
     const user = await User.findOne({ _id: id });
     if (!user) {
-      res.status(400).json({ message: "User not found" });
-      return;
+      return next(new ApiError("User not found", 400));
     }
 
     if (user.token !== token) {
-      res.status(400).json({ message: "Invalid token" });
-      return;
+      return next(new ApiError("Invalid token", 400));
     }
 
     if (
@@ -295,8 +286,7 @@ const resetPassword = asyncHandler(async (req: Request, res: Response) => {
       user.expireDate?.getMinutes() <
         new Date(Date.now() - 1 * 60 * 1000).getMinutes()
     ) {
-      res.status(400).json({ message: "Token expired" });
-      return;
+      return next(new ApiError("Token expired", 400));
     }
     user.password = await bcrypt.hash(password, 12);
     user.token = null;
@@ -306,7 +296,7 @@ const resetPassword = asyncHandler(async (req: Request, res: Response) => {
 
     res.status(200).json({ message: "Password reset successfully" });
   } catch (error) {
-    res.status(500).json({ error: error });
+    return next(new ApiError(`Error resetting password: ${error}`, 500));
   }
 });
 
